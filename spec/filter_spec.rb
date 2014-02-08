939bacec94ad11005026b8fe67945d693d20ca75
Patricio Mac Adden <patriciomacadden@gmail.com>
2014-02-08 15:40:47 -0300

    multiple lets are not allowed

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -4,108 +4,110 @@ describe Hobbit::Filter do
   include Hobbit::Contrib::Mock
   include Rack::Test::Methods
 
-  let(:app) do
-    mock_app do
-      include Hobbit::Filter
-
-      before do
-        env['hobbit.before'] = 'this is before'
-      end
-
-      get '/' do
-        'GET /'
-      end
-
-      after do
-        env['hobbit.after'] = 'this is after'
-      end
-    end
-  end
-
-  %w(after before).each do |kind|
-    str = <<EOS
-  describe '::#{kind}' do
-    specify do
-      p = Proc.new { 'do something' }
-      app = mock_app do
+  describe 'basic specs' do
+    let(:app) do
+      mock_app do
         include Hobbit::Filter
-        #{kind}('', &p)
+
+        before do
+          env['hobbit.before'] = 'this is before'
+        end
+
+        get '/' do
+          'GET /'
+        end
+
+        after do
+          env['hobbit.after'] = 'this is after'
+        end
+      end
+    end
+
+    %w(after before).each do |kind|
+      str = <<EOS
+    describe '::#{kind}' do
+      specify do
+        p = Proc.new { 'do something' }
+        app = mock_app do
+          include Hobbit::Filter
+          #{kind}('', &p)
+        end
+
+        app.to_app.class.filters[:#{kind}].size.must_equal 1
+        app.to_app.class.filters[:#{kind}].first[:block].call.must_equal p.call
+      end
+    end
+
+    describe 'when a filter matches' do
+      it "must call the filters' block" do
+        get '/'
+        last_response.must_be :ok?
+        last_request.env.must_include 'hobbit.#{kind}'
+        last_request.env['hobbit.#{kind}'].must_equal 'this is #{kind}'
+      end
+    end
+EOS
+      class_eval str
+    end
+
+    describe '::filters' do
+      it 'must return a Hash' do
+        app.to_app.class.filters.must_be_kind_of Hash
+      end
+    end
+
+    describe '::compile_filter' do
+      let(:block) { block = Proc.new { |env| [200, {}, []] } }
+
+      it 'must compile /' do
+        path = '/'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/$/.to_s
       end
 
-      app.to_app.class.filters[:#{kind}].size.must_equal 1
-      app.to_app.class.filters[:#{kind}].first[:block].call.must_equal p.call
-    end
-  end
+      it 'must compile with .' do
+        path = '/route.json'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/route.json$/.to_s
+      end
 
-  describe 'when a filter matches' do
-    it "must call the filters' block" do
+      it 'must compile with -' do
+        path = '/hello-world'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/hello-world$/.to_s
+      end
+
+      it 'must compile with params' do
+        path = '/hello/:name'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/hello\/([^\/?#]+)$/.to_s
+
+        path = '/say/:something/to/:someone'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/say\/([^\/?#]+)\/to\/([^\/?#]+)$/.to_s
+      end
+
+      it 'must compile with . and params' do
+        path = '/route/:id.json'
+        route = app.to_app.class.send :compile_filter, path, &block
+        route[:block].call({}).must_equal block.call({})
+        route[:compiled_path].to_s.must_equal /^\/route\/([^\/?#]+).json$/.to_s
+      end
+    end
+
+    it 'must call before and after filters' do
       get '/'
       last_response.must_be :ok?
-      last_request.env.must_include 'hobbit.#{kind}'
-      last_request.env['hobbit.#{kind}'].must_equal 'this is #{kind}'
+      last_request.env.must_include 'hobbit.before'
+      last_request.env['hobbit.before'].must_equal 'this is before'
+      last_request.env.must_include 'hobbit.after'
+      last_request.env['hobbit.after'].must_equal 'this is after'
     end
-  end
-EOS
-    class_eval str
-  end
-
-  describe '::filters' do
-    it 'must return a Hash' do
-      app.to_app.class.filters.must_be_kind_of Hash
-    end
-  end
-
-  describe '::compile_filter' do
-    let(:block) { block = Proc.new { |env| [200, {}, []] } }
-
-    it 'must compile /' do
-      path = '/'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/$/.to_s
-    end
-
-    it 'must compile with .' do
-      path = '/route.json'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/route.json$/.to_s
-    end
-
-    it 'must compile with -' do
-      path = '/hello-world'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/hello-world$/.to_s
-    end
-
-    it 'must compile with params' do
-      path = '/hello/:name'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/hello\/([^\/?#]+)$/.to_s
-
-      path = '/say/:something/to/:someone'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/say\/([^\/?#]+)\/to\/([^\/?#]+)$/.to_s
-    end
-
-    it 'must compile with . and params' do
-      path = '/route/:id.json'
-      route = app.to_app.class.send :compile_filter, path, &block
-      route[:block].call({}).must_equal block.call({})
-      route[:compiled_path].to_s.must_equal /^\/route\/([^\/?#]+).json$/.to_s
-    end
-  end
-
-  it 'must call before and after filters' do
-    get '/'
-    last_response.must_be :ok?
-    last_request.env.must_include 'hobbit.before'
-    last_request.env['hobbit.before'].must_equal 'this is before'
-    last_request.env.must_include 'hobbit.after'
-    last_request.env['hobbit.after'].must_equal 'this is after'
   end
 
   describe 'when multiple filters are declared' do
